1449d5b35b88e1881934cc2ed78e924a6f776d04
Dharanidaran <dharani@ravis-imac.hhk.dk>
2016-08-07 21:01:26 +0200

    Custom Template Tag

--- src/products/templatetags/get_thumbnail.py
+++ src/products/templatetags/get_thumbnail.py
@@ -3,12 +3,22 @@ from django import template
 register = template.Library()
 
 
+from ..models import Product, THUMB_CHOICES
 
 
+arg = arg.lower()
 @register.filter
 def get_thumbnail(obj,arg):
 
 	''' obj =  product instance '''
 
+	if not isinstance(obj, Product):
+		raise TypeError ("This is not a valid product model.")
+
+	choices = dict(THUMB_CHOICES)
+	print(choices)
+
+	if not choices.get(arg):
+		raise TypeError("This is not a valid type for this model")
 	return obj.thumbnail_set.filter(type=arg).first().media.url
 
